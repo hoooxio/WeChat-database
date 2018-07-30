2e6bc6d948a1d99b41e5040ff825e8c3959f031f
hox <h@hooox.io>
2018-07-31 01:01:26 +0800

    1. 数据库直接根据 uin 进行定位，无需撞库 2. 删除多余代码

--- app/src/main/java/com/lixin/wechat/MainActivity.java
+++ app/src/main/java/com/lixin/wechat/MainActivity.java
@@ -1,7 +1,11 @@
 package com.lixin.wechat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -27,16 +31,16 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
     public static final String WX_ROOT_PATH = "/data/data/com.tencent.mm/";
+    public static final int REQUEST_READ_PHONE_STAT = 1;
 
     // U ID 文件路径
     private static final String WX_SP_UIN_PATH = WX_ROOT_PATH + "shared_prefs/auth_info_key_prefs.xml";
 
 
-
     private String mDbPassword;
-
 
 
     private static final String WX_DB_DIR_PATH = WX_ROOT_PATH + "MicroMsg";
@@ -49,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     // 提交参数
-    private int  count=0;
+    private int count = 0;
 
     private String IMEI;
 
@@ -58,29 +62,29 @@ public class MainActivity extends AppCompatActivity {
 
     private Thread type;
 
-   // private EditText link;
+    // private EditText link;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // 获取root权限
         execRootCmd("chmod -R 777 " + WX_ROOT_PATH);
-        execRootCmd("chmod  777 /data/data/com.tencent.mm/shared_prefs/auth_info_key_prefs.xml");
-
 
         // 获取微信的U id
         initCurrWxUin();
 
-
-
         // 获取 IMEI 唯一识别码
         TelephonyManager phone = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        IMEI = phone.getDeviceId();
+        // 当没有权限时，向用户申请权限。忘了x.0之后的系统需要申请权限才可以
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_READ_PHONE_STAT);
+        } else {
+            IMEI = phone.getDeviceId();
+        }
 
-        System.out.println("IMEI"+IMEI);
+        System.out.println("IMEI" + IMEI);
 
         // 根据imei和uin生成的md5码，获取数据库的密码（去前七位的小写字母）
         initDbPassword(IMEI, Uin);
@@ -89,23 +93,40 @@ public class MainActivity extends AppCompatActivity {
 
         System.out.println("开始统计好友数量");
 
-
         //  递归查询微信本地数据库文件
-        File wxDataDir = new File(WX_DB_DIR_PATH);
-        mWxDbPathList.clear();
-        searchFile(wxDataDir, WX_DB_FILE_NAME);
+        StringBuilder sb = new StringBuilder(100);
+        // 当前 uin 所在的数据库位置为 /data/data/com.tencent.mm/MicroMsg/MD5("mm" + uin)/EnMicroMsg.db
+        sb.append(WX_DB_DIR_PATH).append("/").append(getMD5("mm" + Uin)).append("/").append(WX_DB_FILE_NAME);
+        System.out.println("当前 uin 数据库位置：" + sb.toString());
+        File file = new File(sb.toString());
+        if (!file.exists()) {
+            LogUtil.e("该 uin 对应的数据库 " + sb.toString() + "不存在");
+            return;
+        }
 
-        System.out.println("查询数据库文件");
-        //处理多账号登陆情况
-        for (int i = 0; i < mWxDbPathList.size(); i++) {
-            File file = mWxDbPathList.get(i);
-            String copyFilePath = mCurrApkPath + COPY_WX_DATA_DB;
-            //将微信数据库拷贝出来，因为直接连接微信的db，会导致微信崩溃
-            copyFile(file.getAbsolutePath(), copyFilePath);
-            File copyWxDataDb = new File(copyFilePath);
-            openWxDb(copyWxDataDb);
+        // 将微信数据库拷贝一个新的出来，如果直接操作微信数据库会导致微信崩溃
+        String copyFilePath = mCurrApkPath + COPY_WX_DATA_DB;
+        copyFile(sb.toString(), copyFilePath);
+        File copyFile = new File(copyFilePath);
+        openWxDb(copyFile);
+    }
+
+    @Override
+    @SuppressWarnings("all")
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_READ_PHONE_STAT:
+                // 授权成功时回调：设置 IMEI
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    TelephonyManager tm = (TelephonyManager) this.getSystemService(TELEPHONY_SERVICE);
+                    IMEI = tm.getDeviceId();
+                }
+                break;
+            default:
+                break;
         }
     }
+
     /**
      * 执行linux指令
      *
@@ -128,7 +149,6 @@ public class MainActivity extends AppCompatActivity {
             localException.printStackTrace();
         }
     }
-
 
 
     /**
@@ -155,6 +175,7 @@ public class MainActivity extends AppCompatActivity {
             LogUtil.e("获取微信uid失败，请检查auth_info_key_prefs文件权限");
         }
     }
+
     /**
      * 根据imei和uin生成的md5码，获取数据库的密码（去前七位的小写字母）
      *
@@ -168,91 +189,33 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         String md5 = getMD5(imei + uin);
-        System.out.println(imei+uin+"初始数值");
-        System.out.println(md5+"MD5");
+        System.out.println(imei + uin + "初始数值");
+        System.out.println(md5 + "MD5");
         String password = md5.substring(0, 7).toLowerCase();
-        System.out.println("加密后"+password);
+        System.out.println("加密后" + password);
         mDbPassword = password;
     }
 
-    public String getMD5(String info)
-    {
-        try
-        {
+    public String getMD5(String info) {
+        try {
             MessageDigest md5 = MessageDigest.getInstance("MD5");
             md5.update(info.getBytes("UTF-8"));
             byte[] encryption = md5.digest();
 
             StringBuffer strBuf = new StringBuffer();
-            for (int i = 0; i < encryption.length; i++)
-            {
-                if (Integer.toHexString(0xff & encryption[i]).length() == 1)
-                {
+            for (int i = 0; i < encryption.length; i++) {
+                if (Integer.toHexString(0xff & encryption[i]).length() == 1) {
                     strBuf.append("0").append(Integer.toHexString(0xff & encryption[i]));
-                }
-                else
-                {
+                } else {
                     strBuf.append(Integer.toHexString(0xff & encryption[i]));
                 }
             }
 
             return strBuf.toString();
-        }
-        catch (NoSuchAlgorithmException e)
-        {
+        } catch (NoSuchAlgorithmException e) {
             return "";
-        }
-        catch (UnsupportedEncodingException e)
-        {
+        } catch (UnsupportedEncodingException e) {
             return "";
-        }
-    }
-
-    /**
-     * md5加密
-     *
-     * @param content
-     * @return
-     */
-    private String md5(String content) {
-        MessageDigest md5 = null;
-        try {
-            md5 = MessageDigest.getInstance("MD5");
-            md5.update(content.getBytes("UTF-8"));
-            byte[] encryption = md5.digest();//加密
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < encryption.length; i++) {
-                if (Integer.toHexString(0xff & encryption[i]).length() == 1) {
-                    sb.append("0").append(Integer.toHexString(0xff & encryption[i]));
-                } else {
-                    sb.append(Integer.toHexString(0xff & encryption[i]));
-                }
-            }
-            return sb.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /**
-     * 递归查询微信本地数据库文件
-     *
-     * @param file     目录
-     * @param fileName 需要查找的文件名称
-     */
-    private void searchFile(File file, String fileName) {
-        if (file.isDirectory()) {
-            File[] files = file.listFiles();
-            if (files != null) {
-                for (File childFile : files) {
-                    searchFile(childFile, fileName);
-                }
-            }
-        } else {
-            if (fileName.equals(file.getName())) {
-                mWxDbPathList.add(file);
-            }
         }
     }
 
@@ -279,9 +242,9 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             System.out.println("复制单个文件操作出错");
             e.printStackTrace();
-
         }
     }
+
     /**
      * 连接数据库
      *
@@ -300,106 +263,38 @@ public class MainActivity extends AppCompatActivity {
         };
 
         try {
-            //打开数据库连接
-            SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(dbFile, mDbPassword, null, hook);
+            doQuery(dbFile, hook);
 
-            //查询所有联系人（verifyFlag!=0:公众号等类型，群里面非好友的类型为4，未知类型2）
-            // Cursor c1 = db.rawQuery("select * from rcontact where verifyFlag = 0  and type != 4 and type != 2 and type !=33 limit 20, 9999", null);
-            Cursor c1 = db.rawQuery("select * from rcontact where username not like 'gh_%' and verifyFlag<>24 and verifyFlag<>29 and verifyFlag<>56 and type<>33 and type<>70 and verifyFlag=0 and type<>4 and type<>0 and showHead<>43 and type<>65536",null);
-
-            while (c1.moveToNext()) {
-                String type = c1.getString(c1.getColumnIndex("type"));
-                System.out.println(type+"参数");
-                count++;
-
-
-
-            }
-
-            System.out.println("总共参数"+count);
-            //  Toast.makeText(getApplicationContext(),"好友总数"+count,Toast.LENGTH_SHORT).show();
-            c1.close();
-            db.close();
         } catch (Exception e) {
             LogUtil.e("读取数据库信息失败 尝试MEID破解");
 //            e.printStackTrace();
             //打开数据库连接
-            // 根据imei和uin生成的md5码，获取数据库的密码（去前七位的小写字母）
-
-
             // 请自行添加自己手机的MEID  MEID 无法直接获取
-              initDbPassword("A100004AF6C883",  Uin);
+            initDbPassword("A0000069594FA0", Uin);
 
-
-            /*  String MEID=readFileSdcard("/mnt/sdcard/meid.txt");
-            String dMEID=MEID.replace("\r\n","");
-            initDbPassword(dMEID.toUpperCase(),Uin);*/
-
-            System.out.println(mDbPassword+"MEID---密码");
-            count=0;
-            SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(dbFile, mDbPassword, null, hook);
-
-            //查询所有联系人（verifyFlag!=0:公众号等类型，群里面非好友的类型为4，未知类型2）
-            // Cursor c1 = db.rawQuery("select * from rcontact where verifyFlag = 0  and type !=
-            // 4 and type != 2 and type !=33 limit 20, 9999", null);
-
-            // 查询联系人 总数量
-            Cursor c1 = db.rawQuery("select * from rcontact where username not like 'gh_%' and " +
-                    "verifyFlag<>24 and verifyFlag<>29 and verifyFlag<>56 and type<>33 and type<>70 and " +
-                    "verifyFlag=0 and type<>4 and type<>0 and showHead<>43 and type<>65536",null);
-
-            while (c1.moveToNext()) {
-                String type = c1.getString(c1.getColumnIndex("type"));
-                System.out.println(type+"参数");
-                count++;
-
-                //Toast.makeText(getApplicationContext(),type,Toast.LENGTH_SHORT).show();
-
-            }
-            // Toast.makeText(getApplicationContext(),"好友总数"+count,Toast.LENGTH_SHORT).how();
-            System.out.println("总共参数"+count);
-            Toast.makeText(this,"好友总数"+count,Toast.LENGTH_SHORT).show();
-            c1.close();
-            db.close();
-
-
-
-
-
+            System.out.println(mDbPassword + "MEID---密码");
+            count = 0;
+            doQuery(dbFile, hook);
         }
     }
 
+    private void doQuery(File dbFile, SQLiteDatabaseHook hook) {
+        //打开数据库连接
+        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(dbFile, mDbPassword, null, hook);
 
+        //查询所有联系人（verifyFlag!=0:公众号等类型，群里面非好友的类型为4，未知类型2）
+        // Cursor c1 = db.rawQuery("select * from rcontact where verifyFlag = 0  and type != 4 and type != 2 and type !=33 limit 20, 9999", null);
+        Cursor c1 = db.rawQuery("select * from rcontact where username not like 'gh_%' and verifyFlag<>24 and verifyFlag<>29 and verifyFlag<>56 and type<>33 and type<>70 and verifyFlag=0 and type<>4 and type<>0 and showHead<>43 and type<>65536", null);
 
- /*   /*//*//**//*读在/mnt/sdcard/目录下面的文件
-    public String readFileSdcard(String fileName){
-
-        String res="";
-
-        try{
-
-            FileInputStream fin = new FileInputStream(fileName);
-
-            int length = fin.available();
-
-            byte [] buffer = new byte[length];
-
-            fin.read(buffer);
-
-            res = EncodingUtils.getString(buffer, "UTF-8");
-
-            fin.close();
-
+        while (c1.moveToNext()) {
+            String type = c1.getString(c1.getColumnIndex("type"));
+            System.out.println(type + "参数");
+            count++;
         }
 
-        catch(Exception e){
-
-            e.printStackTrace();
-
-        }
-
-        return res;
-
-    }*/
-
+        System.out.println("总共参数" + count);
+        Toast.makeText(getApplicationContext(), "好友总数" + count, Toast.LENGTH_SHORT).show();
+        c1.close();
+        db.close();
+    }
 }
